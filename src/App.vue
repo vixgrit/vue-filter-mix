633d172ca5df11c0f173b0695b5f5cd84dde6513
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-08 23:48:14 -0400

    create and use mixin

--- src/App.vue
+++ src/App.vue
@@ -9,33 +9,32 @@
         <ul>
           <li v-for="fruit in filteredFruits"> {{ fruit }}</li>
         </ul>
+        <hr>
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-      data() {
-        return {
-          text: 'Hello there!',
-          fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-          filterText: ''
-        }
-      },
-      filters: {
-        toUppercase(value) {
-          return value.toUpperCase();
-        }
-      },
-      computed: {
-        filteredFruits() {
-          return this.fruits.filter((element) => {
-            return element.match(this.filterText);
-          });
-        }
+  import List from './List.vue';
+  import { fruitMixin } from './fruitMixin';
+  export default {
+    mixins: [fruitMixin],
+    components: {
+      'appList': List
+    },
+    data() {
+      return {
+        text: 'Hello there!',
+      }
+    },
+    filters: {
+      toUppercase(value) {
+        return value.toUpperCase();
       }
     }
+  }
 </script>
 
 <style>
